aee5e421193a707921e5f988ec489a0f3d6056b8
Bilal Aamer <bilalahmedaamer@gmail.com>
2021-12-10 14:08:39 +0530

    Additional Screens

---
+++ app/lib/main_page.dart
@@ -0,0 +1,17 @@
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'depoo',
+      home: HomePage(),
+    );
+  }
+}
