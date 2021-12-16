0bfa9a6fdac59b718a13672b5a7a400ae1b42845
hrushi20 <hrushikesh20thegreat@gmail.com>
2021-12-16 21:42:21 +0530

    Added env files for twilio

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const dotenv = require('dotenv').config();
 
 
 const twilioRouter = require("./routes/twilio");
